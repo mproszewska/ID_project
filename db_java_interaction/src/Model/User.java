667f46c9ca571226f6f6acfe03df182b41facf21
Unknown <dociebieaniuszlem@gmail.com>
2018-06-08 19:28:00 +0200

    Everything has changed and improved

---
+++ db_java_interaction/src/Model/User.java
@@ -0,0 +1,99 @@
+package Model;
+
+import DB.QueriesMachine;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class User implements QueriesMachine.Selectable, QueriesMachine.Insertable {
+    private Integer userId;
+    private String name;
+    private String surname;
+    private String sex;
+    private Date birthday;
+
+    public User setBirthday(Date birthday) {
+        this.birthday = birthday;
+        return this;
+    }
+
+    public User setBirthday(String date){
+        DateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+        try {
+            this.birthday = format.parse(date);
+        }
+        catch (ParseException p){
+            throw new RuntimeException("unparsable :(((");
+        }
+        return this;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public User setSex(String sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    public User setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public User setUserId(Integer userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public void setMeFromResult(ResultSet resultSet) throws SQLException {
+        userId = resultSet.getInt(1);
+        name = resultSet.getString(2);
+        surname = resultSet.getString(3);
+        sex = resultSet.getString(4);
+        birthday = resultSet.getDate(5);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + " " + birthday + " (" + (Objects.equals(sex, "m") ?"male)" : "female)");
+    }
+
+    @Override
+    public String[] getMySignature() {
+        return new String[]{"name", "surname", "sex", "birthday"};
+    }
+
+    @Override
+    public Object[] getMyValues() {
+        return new Object[]{name, surname, sex, birthday};
+    }
+}
