87bc997c0da40aa592166fcd80c928535d2d7bee
Unknown <dociebieaniuszlem@gmail.com>
2018-06-08 19:51:21 +0200

    Small improvements

--- db_java_interaction/src/DB/Test.java
+++ db_java_interaction/src/DB/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String... arg){
         QueriesMachine machine = new QueriesMachine();
         System.out.println(machine.getUsers());
-        System.out.println(machine.getUser("Dzwonisław", "Krzakiel"));
+//        System.out.println(machine.getUser("Dzwonisław", "Krzakiel"));
         System.out.println(machine.select("select * from users", SelectContainer.class));
     }
 }
