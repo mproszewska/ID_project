fe3fe41589d62bf519c8fbed4a06e19f56c3c3e9
Unknown <dociebieaniuszlem@gmail.com>
2018-06-09 12:02:19 +0200

    Small further improvements

--- concat.sh
+++
@@ -1,2 +0,0 @@
-cat heartrates.sql sleeps.sql supplies.sql sessions.sql user_session.sql > generated_data.sql
-rm heartrates.sql sleeps.sql supplies.sql sessions.sql user_session.sql range.txt
--- generate_fresh.sh
+++ generate_fresh.sh
@@ -7,15 +7,16 @@ echo "concatening..."
 ./concat.sh
 # psql "$1" < ./sql/drop.sql
 echo "rebuilding database..."
-psql "$1" < ./sql/tables.sql --quiet > /dev/null 2>&1
-echo "injecting triggers..."
-# psql "$1" < ./sql/triggers.sql
+psql "$1" < ./sql/tables.sql #--quiet > /dev/null 2>&1
 echo "injecting functions..."
-psql "$1" < ./sql/functions.sql --quiet > /dev/null 2>&1
-psql "$1" < ./sql/views_rules.sql --quiet > /dev/null 2>&1
+psql "$1" < ./sql/functions.sql #--quiet > /dev/null 2>&1
+psql "$1" < ./sql/views_rules.sql #--quiet > /dev/null 2>&1
 echo "injecting data..."
-psql "$1" < ./sql/data.sql --quiet > /dev/null 2>&1
-psql "$1" < ./generated_data.sql --quiet > /dev/null 2>&1
-rm generated_data.sql --quiet > /dev/null 2>&1
+psql "$1" < ./sql/data.sql #--quiet > /dev/null 2>&1
+psql "$1" < ./generated_data.sql #--quiet > /dev/null 2>&1
+echo "injecting triggers..."
+psql "$1" < ./sql/triggers.sql
+echo "removing trash..."
+rm generated_data.sql > /dev/null 2>&1
 echo "finished!"
 
