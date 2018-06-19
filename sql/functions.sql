ce9f2e02293671235cf1d99150a6f5b0b0ac786d
Unknown <dociebieaniuszlem@gmail.com>
2018-06-19 07:21:26 +0200

    Another bug

--- sql/functions.sql
+++ sql/functions.sql
@@ -518,3 +518,204 @@ END;
 $f$
 LANGUAGE plpgsql;
 
+
+----
+DROP FUNCTION IF EXISTS get_my_med(INTEGER,DATE) CASCADE;
+CREATE OR REPLACE FUNCTION get_my_med(id INTEGER, day DATE)
+  RETURNS CHARACTER VARYING[] AS
+$$
+SELECT ARRAY(SELECT DISTINCT name
+             FROM user_medication um
+               JOIN medications m2 ON um.medication_id = m2.medication_id
+             WHERE user_id = id AND um.date :: DATE = day
+             ORDER BY name)
+$$
+LANGUAGE SQL;
+
+----
+CREATE OR REPLACE VIEW activities_meds
+  AS
+    SELECT
+      user_id,
+      s.start_time :: DATE,
+      extract(EPOCH FROM (s.end_time - s.start_time))/60 as duration,
+      a.activity_id,
+      us.distance,
+      description,
+      get_my_med(user_id, s.start_time :: DATE) as medications
+    FROM user_session us
+      JOIN sessions s ON us.session_id = s.session_id
+      JOIN activities a ON s.activity_id = a.activity_id;
+
+----
+DROP FUNCTION IF EXISTS get_best_stuff(INTEGER) CASCADE;
+CREATE OR REPLACE FUNCTION get_best_stuff(act_id INTEGER)
+  RETURNS TABLE(medications CHARACTER VARYING[],avg_result NUMERIC) AS
+$$
+SELECT
+  a.medications,
+  round(coalesce(avg(distance), avg(duration))::NUMERIC, 2) as result
+FROM activities_meds a
+WHERE activity_id = act_id
+GROUP BY activity_id, medications
+ORDER BY result DESC LIMIT 1;
+$$
+LANGUAGE sql;
+
+----
+CREATE VIEW best_medication_sets(activity, medication_set, result) AS
+  SELECT
+    a.name,
+    (select gb.medications from get_best_stuff(a.activity_id) gb),
+    (select gb.avg_result from get_best_stuff(a.activity_id) gb)
+  FROM activities a;
+
+----
+CREATE OR REPLACE VIEW weight_differences(user_id, month, difference_sum) AS
+  SELECT
+    user_id,
+    to_char("date", 'Month'),
+    max(weight)-min(weight)
+  FROM height_weight hw
+  GROUP BY user_id, to_char("date", 'Month')
+  ORDER BY 1, 2;
+
+----
+CREATE OR REPLACE VIEW months_weight_differences(month, difference_avg) AS
+  SELECT
+    month,
+    avg(difference_sum)
+  FROM weight_differences
+  GROUP BY month
+  ORDER BY  avg(difference_sum) DESC;
+
+----
+CREATE OR REPLACE VIEW activities_sleep_avg_result AS
+  SELECT
+    a.name,
+    avg(coalesce(distance, extract(EPOCH FROM (ses.end_time - ses.start_time))/60) ) result,
+    to_char(s.end_time - s.start_time, 'hh:MIh') sleep_duration
+  FROM user_session us
+  JOIN sessions ses ON us.session_id = ses.session_id
+  JOIN activities a ON ses.activity_id = a.activity_id
+  JOIN sleep s ON us.user_id = s.user_id AND ses.start_time::DATE = s.start_time::DATE
+  GROUP BY name, sleep_duration
+  ORDER BY name, 2 DESC;
+
+----
+CREATE OR REPLACE FUNCTION get_best_sleeps(act_id INTEGER)
+  RETURNS TABLE(sleep_time VARCHAR, result NUMERIC) AS
+$$
+SELECT
+  sleep_duration,
+  result::NUMERIC
+FROM activities_sleep_avg_result asr
+JOIN activities a ON a.name = asr.name
+WHERE act_id = a.activity_id
+ORDER BY result DESC
+LIMIT 1;
+$$
+LANGUAGE sql;
+
+----
+CREATE OR REPLACE VIEW best_sleep_time AS
+  SELECT
+    a.name,
+    (select gb.sleep_time from get_best_sleeps(a.activity_id) gb),
+    round((select gb.result from get_best_sleeps(a.activity_id) gb),2) "avg distance/time"
+  FROM activities a;
+
+----
+DROP FUNCTION IF EXISTS section_ranking(INTEGER,DATE,DATE) CASCADE;
+CREATE OR REPLACE FUNCTION section_ranking(sectionid INTEGER, start_0 DATE, end_0 DATE)
+ RETURNS TABLE(user_id INTEGER, name VARCHAR,age NUMERIC,attendance bigint,distance NUMERIC,kcal NUMERIC,injured BOOL) AS $$
+
+ SELECT user_id,
+(
+  SELECT CONCAT(name,' ',surname)
+    FROM users
+    WHERE us.user_id=user_id
+),
+get_age(user_id,start_0),
+(
+  SELECT CASE WHEN COUNT(*)>0
+    THEN COUNT(*)/(SELECT COUNT(*) FROM sessions WHERE section_id=sectionid AND start_time::DATE<= end_0 AND end_time::DATE >= start_0) ELSE 0 END
+  FROM user_session
+    LEFT JOIN sessions s USING (session_id)
+  WHERE us.user_id=user_id AND section_id=sectionid AND start_time::DATE<= end_0 AND end_time::DATE >= start_0
+),
+(
+  SELECT SUM(distance)
+    FROM user_session
+      LEFT JOIN sessions USING (session_id)
+    WHERE user_id=us.user_id AND start_time::DATE<= end_0 AND end_time::DATE >= start_0
+),
+kcal(user_id,start_0::TIMESTAMP,(end_0+INTERVAL '1 day')::TIMESTAMP),
+CASE
+  WHEN (
+    SELECT user_id
+      FROM injuries
+      WHERE user_id=us.user_id AND "date"+duration>=start_0 AND "date"<=end_0
+    ) IS NOT NULL
+  THEN true
+  ELSE false
+END
+FROM user_section us WHERE section_id=sectionid AND start_time::DATE<= end_0 AND COALESCE(end_time,start_0)::DATE >= start_0 ;
+$$
+LANGUAGE sql;
+
+
+----
+CREATE OR REPLACE FUNCTION get_users_number_from_section(sectionid INTEGER)
+  RETURNS INTEGER AS
+$$
+SELECT
+  count (*)::INTEGER
+FROM user_section ss
+WHERE section_id = sectionid
+GROUP BY section_id;
+$$
+LANGUAGE sql;
+
+----
+CREATE OR REPLACE FUNCTION get_users_from_section(sectionid INTEGER)
+  RETURNS TABLE (name VARCHAR) AS
+$$
+SELECT
+  u.name || ' ' || u.surname as name
+FROM users u
+JOIN user_section ss ON u.user_id = ss.user_id
+WHERE section_id = sectionid AND ss.end_time ISNULL
+ORDER BY u.surname, u.name;
+$$
+LANGUAGE sql;
+
+
+----
+CREATE OR REPLACE FUNCTION get_meetings_number(sectionid INTEGER)
+  RETURNS INTEGER AS
+$$
+SELECT
+  count(*)::INTEGER
+FROM sessions s
+WHERE sectionid = s.section_id
+GROUP BY section_id;
+$$
+LANGUAGE sql;
+
+
+----
+DROP VIEW IF EXISTS sections_info;
+CREATE OR REPLACE VIEW sections_info AS
+  SELECT
+    s.name,
+    city,
+    u.name || ' ' || u.surname as trener,
+    a.name as sport,
+    get_users_number_from_section(s.section_id) as members,
+    get_meetings_number(s.section_id) as meetings_number
+  FROM sections s
+  JOIN users u ON s.trainer_id = u.user_id
+  JOIN activities a ON s.activity_id = a.activity_id
+  ORDER BY s.section_id;
+
